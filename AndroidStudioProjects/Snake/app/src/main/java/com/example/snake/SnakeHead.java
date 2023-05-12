1e4808b1ce0048214a1ae13e0a5d5f9da23b6ca9
ritikjain2117 <211b258@juetguna.in>
2023-05-12 22:22:34 +0530

    updated head

--- AndroidStudioProjects/Snake/app/src/main/java/com/example/snake/SnakeHead.java
+++ AndroidStudioProjects/Snake/app/src/main/java/com/example/snake/SnakeHead.java
@@ -19,8 +19,8 @@ public class SnakeHead {
     public int getHead(){
         return Head;
     }
-    public int getHead2(){
-        return Head2;
+    public int getHead3(){
+        return Head3;
     }
     public int getTail(){
         return Tail;
